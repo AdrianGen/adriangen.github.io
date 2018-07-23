88b8b1669264b23fc51dcc405139e28ef67dc29e
Unknown <vagrant1165@gmail.com>
2018-07-23 22:21:03 +0100

    css and some js updates + html cosmetics

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// ProgressBar animation
+
+$(".progressBar > span").each(function() {
+  $(this)
+    .data("origWidth", $(this).width())
+    .width(0)
+    .animate({
+      width: $(this).data("origWidth")
+    }, 1200);
+});
