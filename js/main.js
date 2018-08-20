1e0a051c048ea8c6141486c4add3ede40d714fac
Unknown <vagrant1165@gmail.com>
2018-08-20 19:03:59 +0100

    words

--- js/main.js
+++ js/main.js
@@ -8,3 +8,21 @@ $(".progressBar > span").each(function() {
       width: $(this).data("origWidth")
     }, 1200);
 });
+
+// Intersection Observer
+
+// var target = document.querySelector('.progressBar > span');
+// observer.observe(target);
+
+// function handleIntersect(entries, observer) {
+//   entries.forEach(function(entry) {
+//     if (entry.intersectionRatio > prevRatio) {
+//       entry.target.style.backgroundColor = increasingColor.replace("ratio", entry.intersectionRatio);
+//     } else {
+//       entry.target.style.backgroundColor = decreasingColor.replace("ratio", entry.intersectionRatio);
+//     }
+
+//     prevRatio = entry.intersectionRatio;
+//   });
+// }
+
